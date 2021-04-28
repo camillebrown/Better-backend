265e3149379262c713d4570567af22758c14477c
Camille Brown <camillebrown@Camilles-MacBook-Pro.local>
2021-04-28 15:10:59 -0700

    fixed global variable

--- resources/users.py
+++ resources/users.py
@@ -1,6 +1,6 @@
 import models
 
-from flask import Blueprint, jsonify, request, session
+from flask import Blueprint, jsonify, request, session, g
 from flask_bcrypt import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, current_user, login_required
 from flask.sessions import SecureCookieSessionInterface
